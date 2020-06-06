ef835819450fa4be038fbd22f31b888fa9cf7630
Chotiwut Sattayapisan <ezio.magtap@gmail.com>
2020-06-06 16:05:14 +0700

    another way to fix overfetching

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,13 @@
-import jsonplaceholder from '../apis/jsonplaceholder';
 import _ from 'lodash';
+import jsonplaceholder from '../apis/jsonplaceholder';
+
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId')); 
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 
 export const fetchPosts = () => async dispatch => {
@@ -8,11 +16,18 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);   
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {   
     const response =  await jsonplaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);   
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response =  await jsonplaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
 
