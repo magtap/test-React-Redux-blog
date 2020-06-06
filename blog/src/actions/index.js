6491e7659f9400fd60c8b3799c800b92ccf44cc3
Chotiwut Sattayapisan <ezio.magtap@gmail.com>
2020-06-06 14:53:57 +0700

    create UserHeader to show user name on the screen
    - create new action (fetchUser) to get user id
    -create UserHeader.js

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,4 +5,10 @@ export const fetchPosts = () => async dispatch => {
     const response =  await jsonplaceholder.get('/posts');
     
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
-};   
+};
+
+export const fetchUser = (id) => async dispatch => {
+    const response =  await jsonplaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
